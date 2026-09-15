3defdc3bcb689b3938f7d2e7ceceb800093941e6
Jackson-12345 <jacksonantoine060@gmail.com>
2026-09-14 21:09:48 -0400

    Update task-manager.js

--- js/task-manager.js
+++ js/task-manager.js
@@ -1,1 +1,9 @@
+// weekly task goal
+function weeklyGoal(userName, dailyGoal, bonusTasks) {
+    let weeklyGoal = dailyGoal * 5;
+    let totalGoal = weeklyGoal + bonusTasks;
 
+    let output = `${userName}, your weekly task goal is ${totalGoal} tasks.`;
+
+    document.getElementById("goal-message").innerHTML = output;
+}
